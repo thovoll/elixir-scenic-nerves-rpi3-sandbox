dff9f5071e727d3c44bba894c2a4ba51fee04b7e
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-11-18 10:48:43 -0800

    Moving dot.

--- config/config.host.exs
+++ config/config.host.exs
@@ -3,7 +3,8 @@ use Mix.Config
 config :esn_rpi3_sandbox, :viewport, %{
   name: :main_viewport,
   # default_scene: {EsnRpi3Sandbox.Scene.Crosshair, nil},
-  default_scene: {EsnRpi3Sandbox.Scene.SysInfo, nil},
+  # default_scene: {EsnRpi3Sandbox.Scene.SysInfo, nil},
+  default_scene: {EsnRpi3Sandbox.Scene.MovingDots, nil},
   size: {800, 480},
   opts: [scale: 1.0],
   drivers: [
