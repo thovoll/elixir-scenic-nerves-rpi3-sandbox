99b5df8aa1cc279fbba0076c3b2581bdefa9c315
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-11-21 06:48:34 -0800

    Don't use timer.

--- lib/scenes/moving_dots.ex
+++ lib/scenes/moving_dots.ex
@@ -4,41 +4,46 @@ defmodule EsnRpi3Sandbox.Scene.MovingDots do
   alias Scenic.ViewPort
   import Scenic.Primitives
 
-  @graph Graph.build()
-         |> circle(20, fill: :red, id: :dot)
+  @animate_ms 15
+  @x_speed 10
+  @x_start 1
+  @dot_size 20
 
-  @animate_ms 2
+  @graph Graph.build()
+         |> circle(@dot_size, fill: :red, id: :dot)
 
   def init(_, opts) do
     viewport = opts[:viewport]
     {:ok, %ViewPort.Status{size: {vp_width, vp_height}}} = ViewPort.info(viewport)
-    position = { 30, vp_height / 2 }
+    position = { @x_start, vp_height / 2 }
 
     graph =
       Graph.modify(@graph, :dot, &update_opts(&1, translate: position))
       |> push_graph()
 
-    {:ok, timer} = :timer.send_interval(@animate_ms, :animate)
-
     state = %{
       viewport: viewport,
-      timer: timer,
       graph: graph,
       position: position,
+      vp_width: vp_width
     }
+
+    Process.send_after(self(), :animate, @animate_ms)
 
     {:ok, state}
   end
 
-  def handle_info(:animate, %{position: p, graph: graph} = state) do
+  def handle_info(:animate, %{position: p, graph: graph, vp_width: vp_width} = state) do
     graph =
       graph
       |> Graph.modify(:dot, &update_opts(&1, translate: p))
       |> push_graph()
 
     { x, y } = p
-    new_x = if x > 800 do 1 else x + 1 end
+    new_x = if x > vp_width do 1 else x + @x_speed end
     new_p = { new_x, y }
+
+    Process.send_after(self(), :animate, @animate_ms)
 
     {:noreply, %{state | graph: graph, position: new_p}}
   end
