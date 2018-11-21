ac049682fa7cf6712b5653c24b5b13717cb3c120
Thomas Vollmer <34384788+thovoll@users.noreply.github.com>
2018-11-21 07:10:59 -0800

    5 dots.

--- lib/scenes/moving_dots.ex
+++ lib/scenes/moving_dots.ex
@@ -10,22 +10,22 @@ defmodule EsnRpi3Sandbox.Scene.MovingDots do
   @dot_size 20
 
   @graph Graph.build()
-         |> circle(@dot_size, fill: :red, id: :dot)
+         |> circle(@dot_size, fill: :red, id: :dot1)
+         |> circle(@dot_size, fill: :red, id: :dot2)
+         |> circle(@dot_size, fill: :red, id: :dot3)
+         |> circle(@dot_size, fill: :red, id: :dot4)
+         |> circle(@dot_size, fill: :red, id: :dot5)
 
   def init(_, opts) do
     viewport = opts[:viewport]
     {:ok, %ViewPort.Status{size: {vp_width, vp_height}}} = ViewPort.info(viewport)
-    position = { @x_start, vp_height / 2 }
-
-    graph =
-      Graph.modify(@graph, :dot, &update_opts(&1, translate: position))
-      |> push_graph()
 
     state = %{
       viewport: viewport,
-      graph: graph,
-      position: position,
-      vp_width: vp_width
+      graph: @graph,
+      vp_width: vp_width,
+      vp_height: vp_height,
+      x_pos: @x_start,
     }
 
     Process.send_after(self(), :animate, @animate_ms)
@@ -33,19 +33,23 @@ defmodule EsnRpi3Sandbox.Scene.MovingDots do
     {:ok, state}
   end
 
-  def handle_info(:animate, %{position: p, graph: graph, vp_width: vp_width} = state) do
+  def handle_info(:animate, %{graph: graph, vp_width: vp_width, vp_height: vp_height, x_pos: x_pos} = state) do
+    y_pos = vp_height / 2 - 80
+    
     graph =
       graph
-      |> Graph.modify(:dot, &update_opts(&1, translate: p))
+      |> Graph.modify(:dot1, &update_opts(&1, translate: { x_pos, y_pos}))
+      |> Graph.modify(:dot2, &update_opts(&1, translate: { x_pos - 40, y_pos + 40}))
+      |> Graph.modify(:dot3, &update_opts(&1, translate: { x_pos - 80, y_pos + 80}))
+      |> Graph.modify(:dot4, &update_opts(&1, translate: { x_pos - 120, y_pos + 120}))
+      |> Graph.modify(:dot5, &update_opts(&1, translate: { x_pos - 160, y_pos + 160}))
       |> push_graph()
 
-    { x, y } = p
-    new_x = if x > vp_width do 1 else x + @x_speed end
-    new_p = { new_x, y }
+    new_x_pos = if x_pos > vp_width + 160 do 1 else x_pos + @x_speed end
 
     Process.send_after(self(), :animate, @animate_ms)
 
-    {:noreply, %{state | graph: graph, position: new_p}}
+    {:noreply, %{state | graph: graph, x_pos: new_x_pos}}
   end
 
   def handle_input(_input, _context, state), do: {:noreply, state}
